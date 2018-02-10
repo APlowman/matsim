f559842ab83ce9d26a7cffc081e0e295322db1b7
APlowman <adam.jp@live.co.uk>
2018-02-10 01:30:20 +0000

    Update __main__ to use getopt

--- matsim/__main__.py
+++ matsim/__main__.py
@@ -1,7 +1,7 @@
 """matsim.__main__.py"""
 
 import sys
-import argparse
+import getopt
 
 import yaml
 
@@ -9,11 +9,30 @@ from matsim import OPTSPEC, MAKESIMS_FN, UPDATE_FN, PROCESS_FN, SEQ_DEFN, parse_
 from matsim.utils import prt
 
 
-def main(args=sys.argv):
+def main(args=sys.argv[1:]):
 
-    if len(args) == 1 or args[1] == 'make':
+    try:
+        long_args = [
+            'make',
+            'load=',
+            'submit=',
+            'process=',
+            'force-process=',
+            'run-group=',
+            'update',
+            'no-update'
+        ]
+        opts, args = getopt.getopt(args, 'ml:s:p:f:r:u', long_args)
+
+    except getopt.GetoptError:
+        print('error')
+        sys.exit(2)
+
+    if opts[0][0] in ('-m', '--make'):
 
         from matsim.simulation import makesims
+
+        print('Make.')
 
         # Parse the makesims.yml options:
         with open(MAKESIMS_FN, 'r') as ms_opts_fp:
@@ -22,40 +41,64 @@ def main(args=sys.argv):
 
         makesims.main(ms_opts)
 
-    elif args[1] == 'load':
+    elif opts[0][0] in ('-l', '--load'):
 
         from matsim.simulation.simgroup import SimGroup
 
-        sim_group = SimGroup.load_state(args[2], 'stage')
+        print('Load.')
+
+        hid = opts[0][1]
+
+        sim_group = SimGroup.load_state(hid, 'stage')
         prt(sim_group, 'sim_group')
 
         print('loaded, now saving...')
 
         sim_group.save_state('stage', path='testy_test')
 
-    elif args[1] == 'submit':
+    elif opts[0][0] in ('-s', '--submit'):
 
         from matsim.simulation.simgroup import SimGroup
 
-        sim_group = SimGroup.load_state(args[2], 'stage')
+        print('Submit.')
 
-        prt(sim_group.run_options, 'run opt')
-
-        run_group_idx = int(args[3])
+        hid = opts[0][1]
+        run_group_idx = None
+        for opt, arg in opts:
+            if opt in ('-r', '--run-group'):
+                run_group_idx = arg
+        sim_group = SimGroup.load_state(hid, 'stage')
         sim_group.submit_run_groups([run_group_idx])
 
-    elif args[1] == 'process':
+    elif opts[0][0] in ('-p', '--process'):
 
         from matsim.simulation import process
         from matsim.simulation.simgroup import SimGroup
 
-        sim_group = SimGroup.load_state(args[2], 'scratch')
-        proc_args = [sim_group]
-        if len(args) == 4:
-            run_group_idx = int(args[3])
-            proc_args += [run_group_idx]
-        process.main(*proc_args)
+        hid = opts[0][1]
+        do_update = True
+        run_group_idx = None
+        for opt, arg in opts:
+            if opt == '--no-update':
+                do_update = False
+            if opt in ('-r', '--run-group'):
+                run_group_idx = arg
+            if opt in ('-f', '--force-process'):
+                force_process_run_idx = [int(i) for i in arg.split(',')]
 
-    elif args[1] == 'update':
+        print('Process: {}. rg_idx: {}. do_update: {}. force_process_run_idx: {}'.format(
+            hid, run_group_idx, do_update, force_process_run_idx))
+
+        sim_group = SimGroup.load_state(hid, 'scratch')
+        proc_args = {
+            'sim_group': hid,
+            'run_group_idx': run_group_idx,
+            'do_update': do_update,
+            'force_process': force_process_run_idx,
+        }
+        process.main(**proc_args)
+
+    elif opts[0][0] in ('-u', '--update'):
+
         from matsim import update
         update.main()
